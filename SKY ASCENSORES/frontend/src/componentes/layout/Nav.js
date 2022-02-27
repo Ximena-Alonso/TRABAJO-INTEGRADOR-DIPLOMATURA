48e8d482206dc0d7c878e256a650beb033cf7808
Ximena Alonso <alonsoxime22@hotmail.com>
2022-02-27 18:05:35 -0300

    nav

--- SKY ASCENSORES/frontend/src/componentes/layout/Nav.js
+++ SKY ASCENSORES/frontend/src/componentes/layout/Nav.js
@@ -6,12 +6,12 @@ const Nav = (props)=>{
         <nav>
             <div className="holder">
                 <ul>
-                    <li><NavLink activeClassName='activo' exact to="/" >Home</NavLink></li>
-                    <li><NavLink activeClassName='activo' exact  to="/Nosotros">Nosotros</NavLink></li>
-                    <li><NavLink activeClassName='activo' exact  to="/Galeria">Galeria</NavLink></li>
-                    <li><NavLink activeClassName='activo' exact  to="/Servicios">Servicios</NavLink></li>
-                    <li><NavLink activeClassName='activo' exact  to="/Normativa">Legislación</NavLink></li>
-                    <li><NavLink activeClassName='activo' exact  to="/Contacto">Contacto</NavLink></li>
+                    <li ><NavLink activeClassName='activo' exact to="/" >Home</NavLink></li>
+                    <li ><NavLink activeClassName='activo' exact  to="/Nosotros">Nosotros</NavLink></li>
+                    <li ><NavLink activeClassName='activo' exact  to="/Galeria">Galeria</NavLink></li>
+                    <li ><NavLink activeClassName='activo' exact  to="/Servicios">Servicios</NavLink></li>
+                    <li ><NavLink activeClassName='activo' exact  to="/Normativa">Legislación</NavLink></li>
+                    <li ><NavLink activeClassName='activo' exact  to="/Contacto">Contacto</NavLink></li>
                     
                 </ul> 
 
