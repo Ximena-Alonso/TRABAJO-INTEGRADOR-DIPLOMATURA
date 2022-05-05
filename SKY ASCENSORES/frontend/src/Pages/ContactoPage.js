17544090f3a83439ceb1d20fa2833a816139c031
Ximena Alonso <alonsoxime22@hotmail.com>
2022-05-05 18:38:01 -0300

    mail

--- SKY ASCENSORES/frontend/src/Pages/ContactoPage.js
+++ SKY ASCENSORES/frontend/src/Pages/ContactoPage.js
@@ -19,7 +19,7 @@ const ContactoPage = (props)=>{
     const handleChange = e=> {
         const {name, value} = e.target;
         setFormData (oldData => ({
-            oldData,
+            ...oldData,
             [name]: value
         }));
     }
