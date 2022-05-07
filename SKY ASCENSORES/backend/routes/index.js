555560f7020d5e56b5d106341cb295ea55344bbb
Ximena Alonso <alonsoxime22@hotmail.com>
2022-05-06 21:33:19 -0300

    mm

--- SKY ASCENSORES/backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* para queinicie en admin/login */
-router.get('/', function(req, res, next) {
-  res.redirect('admin/login');
-});
-
-module.exports = router;
